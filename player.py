634e43da6d89be8790ee932449f9d6c0fbdbebb1
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-24 23:11:11 +0700

    init method

--- player.py
+++ player.py
@@ -5,13 +5,27 @@
 class Player:
     # name = 'Mbappe'
     name = ''
+    speed = ''
 
-    def getName(self, param):
-        self.name = param
+    def __init__(self, param1, param2):
+        self.name = param1
+        self.speed = param2
+
+    def getName(self):
+        # self.name = param
         return self.name
 
+    def getSpeed(self):
+        # self.speed = param
+        return self.speed
+
 # outside class
-actor = Player()
+# actor = Player()
 # print(actor.name)
 # print(actor.getName())
-print(actor.getName('Messi'))
+# print(actor.getName('Messi'))
+
+player1 = Player('Dybala','86')
+player2 = Player('Messi','95')
+print(player1.getName() + " have speed "+ player1.getSpeed())
+print(player2.getName() + " have speed "+ player2.getSpeed())
