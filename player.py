abed870d1ce6ca6f0f08f3e5a40e409d57e80c30
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-24 23:26:35 +0700

    class inheritance

--- player.py
+++ player.py
@@ -19,13 +19,21 @@ class Player:
         # self.speed = param
         return self.speed
 
+class ArgentinaPlayer(Player):
+    def setAge(self, age):
+        self.age = age
+        return self.age
+
 # outside class
 # actor = Player()
 # print(actor.name)
 # print(actor.getName())
 # print(actor.getName('Messi'))
 
-player1 = Player('Dybala','86')
-player2 = Player('Messi','95')
-print(player1.getName() + " have speed "+ player1.getSpeed())
-print(player2.getName() + " have speed "+ player2.getSpeed())
+# player1 = Player('Dybala','86')
+# player2 = Player('Messi','95')
+# print(player1.getName() + " have speed "+ player1.getSpeed())
+# print(player2.getName() + " have speed "+ player2.getSpeed())
+
+player = ArgentinaPlayer('Dybala','86')
+print(player.getName() + " have age " + player.setAge('26'))
